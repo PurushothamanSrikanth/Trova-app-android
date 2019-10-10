d6ac08599d87ff5e1300eca630dfb2d578044e6b
Arshiya Ranjith <arshia.ranjith@gmail.com>
2019-10-10 20:08:18 +0530

    first commit

---
+++ app/src/main/java/com/example/trova/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.trova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView iv2,image1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bt1 = (Button) findViewById(R.id.bt1);
+        iv2 = (ImageView) findViewById(R.id.image1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        Bitmap bitmap =(Bitmap)data.getExtras().get("data");
+        iv2.setImageBitmap(bitmap);
+    }
+}
